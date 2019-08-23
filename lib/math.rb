48621ada58f1faa926eb71c8ea5e2703cdc8bf80
Ted Williams <tboxyz@gmail.com>
2019-08-23 20:36:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+result = num1+num2
+p result
 end
 
 def subtraction(num1, num2)
-
+result = num1-num2
+p result
 end
 
 def division(num1, num2)
-
+result = num1/num2
+p result
 end
 
 def multiplication(num1, num2)
-
+result = num1*num2
+p result
 end
 
 def modulo(num1, num2)
-
+result = num1%num2
+p result
 end
-
+result = num1+num2
 def square_root(num)
+  result= num.sqrt
+  p result
 
 end
